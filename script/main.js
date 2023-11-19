2a8a9c43dd01cf9ceb7846800c4c70459d031b42
John Raiven Olazo <johnraivenolazo@gmail.com>
2023-11-19 15:48:02 +0800

    Applied parallax effect to header

--- script/main.js
+++ script/main.js
@@ -104,3 +104,12 @@ const shuffleArray = (array) => {
 // ========================================================================== //
 // Parallax
 
+const parallax = document.getElementById("parallax");
+
+// Parallax Effect for DIV 1
+window.addEventListener("scroll", function () {
+  let offset = window.pageYOffset;
+  parallax.style.backgroundPositionY = offset * 0.7 + "px";
+  // DIV 1 background will move slower than other elements on scroll.
+});
+
