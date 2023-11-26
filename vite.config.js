bf241a3b34c32ae2edd7bd3e1f7a1abcf1155a99
John Raiven Olazo <johnraivenolazo@gmail.com>
2023-11-27 06:16:57 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,12 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { resolve } from 'path'
 
-export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      scss: {
-        // Specify the entry point for your Sass files
-        additionalData: `@import "@/scss/main.scss";`,
-      },
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        login: resolve(__dirname, 'main/index.html'),
-      },
-    },
-  },
-});
+export default {
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                login: resolve(__dirname, 'main/index.html')
+            }
+        }
+    }
+}
