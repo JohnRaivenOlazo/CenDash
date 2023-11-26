7ab85781e94f8d7dd6422818bd56abcdfd4f821c
John Raiven Olazo <johnraivenolazo@gmail.com>
2023-11-27 05:06:57 +0800

    Update packages

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,19 @@ export default {
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
-                login: resolve(__dirname, 'main/index.html')
+                login: resolve(__dirname, 'main/index.html'),
+                ...globEntries(path.resolve(__dirname, 'src', '*.css'))
             }
         }
     }
 }
+
+function globEntries(globPath) {
+    const glob = require('glob');
+    const entries = {};
+    glob.sync(globPath).forEach((entry) => {
+      const chunkName = path.basename(entry, path.extname(entry));
+      entries[chunkName] = entry;
+    });
+    return entries;
+  }
